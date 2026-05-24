f6d385383daab57bcf08d6c1a92976ca95ea94e7
rkdnrrl <52886645+rkdnrrl@users.noreply.github.com>
2026-05-24 12:01:19 +0900

    auto: update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -50,6 +50,7 @@ router.use('/modules', require('./modules'));
 router.use('/donate', require('./donate'));
 router.use('/announcements', require('./announcements'));
 router.use('/community', require('./community'));
+router.use('/social', require('./social'));
 router.use('/shop', require('./shop'));
 router.use('/leaderboard', require('./leaderboard'));
 router.use('/missions', require('./missions'));
