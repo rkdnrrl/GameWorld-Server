8453283895030abda764346c0aa5a6da0b02a10f
rkdnrrl <52886645+rkdnrrl@users.noreply.github.com>
2026-05-24 17:45:31 +0900

    auto: update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
+const { WebSocketServer } = require('ws');
 const app = require('./app');
 const config = require('./config');
 const { disconnect } = require('./db');
+const { createGameServer } = require('./ws/gameServer');
 
 const server = app.listen(config.port, () => {
   console.log(`[gameworld] listening on :${config.port} (${config.env})`);
 });
 
+// WebSocket 게임 서버 연결
+const wss = new WebSocketServer({ server, path: '/ws/game' });
+createGameServer(wss);
+console.log('[gameworld] WebSocket game server attached at /ws/game');
+
 async function shutdown(signal) {
   console.log(`[gameworld] received ${signal}, shutting down`);
+  wss.close();
   server.close(async () => {
     await disconnect();
     process.exit(0);
@@ -15,7 +23,7 @@ async function shutdown(signal) {
   setTimeout(() => process.exit(1), 10000).unref();
 }
 
-process.on('SIGINT', () => shutdown('SIGINT'));
+process.on('SIGINT',  () => shutdown('SIGINT'));
 process.on('SIGTERM', () => shutdown('SIGTERM'));
 process.on('unhandledRejection', (err) => {
   console.error('[unhandledRejection]', err);
