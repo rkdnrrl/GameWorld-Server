600b0f76cedd013c494718661589ad5c8e985ca6
rkdnrrl <52886645+rkdnrrl@users.noreply.github.com>
2026-05-30 13:44:33 +0900

    q

--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
-const { WebSocketServer } = require('ws');
 const app = require('./app');
 const config = require('./config');
 const { disconnect } = require('./db');
 const { ensureCoreSchema } = require('./db/ensureCoreSchema');
-const { createGameServer } = require('./ws/gameServer');
 
 let server;
-let wss;
 
 async function start() {
   try {
@@ -19,15 +16,10 @@ async function start() {
   server = app.listen(config.port, () => {
     console.log(`[gameworld] listening on :${config.port} (${config.env})`);
   });
-
-  wss = new WebSocketServer({ server, path: '/ws/game' });
-  createGameServer(wss);
-  console.log('[gameworld] WebSocket game server attached at /ws/game');
 }
 
 async function shutdown(signal) {
   console.log(`[gameworld] received ${signal}, shutting down`);
-  if (wss) wss.close();
   if (!server) {
     await disconnect();
     process.exit(0);
